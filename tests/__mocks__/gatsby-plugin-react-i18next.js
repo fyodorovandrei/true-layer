ae59ad4316fe6c95764b67e569bb15dc259ed40a
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-04-03 14:11:32 +0200

    update mock for gatsby plugin i18next

--- tests/__mocks__/gatsby-plugin-react-i18next.js
+++ tests/__mocks__/gatsby-plugin-react-i18next.js
@@ -3,6 +3,14 @@ const { languages, defaultLanguage } = require("../../i18next");
 module.exports = {
     Link: ({ children }) => children,
     Trans: ({ children }) => children,
+    useTranslation: () => {
+        return {
+            t: (str) => str,
+            i18n: {
+                changeLanguage: () => new Promise(() => {}),
+            },
+        };
+    },
     useI18next: () => {
         return {
             languages,
