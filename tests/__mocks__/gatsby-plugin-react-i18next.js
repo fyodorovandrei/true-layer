269df5cbf1edd7ee5d672951a75711183a2b680d
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-04-03 13:12:16 +0200

    update mock languages and default language

--- tests/__mocks__/gatsby-plugin-react-i18next.js
+++ tests/__mocks__/gatsby-plugin-react-i18next.js
@@ -1,10 +1,12 @@
+const { languages, defaultLanguage } = require("../../i18next");
+
 module.exports = {
     Link: ({ children }) => children,
     Trans: ({ children }) => children,
     useI18next: () => {
         return {
-            languages: ["en"],
-            language: "en",
+            languages,
+            language: defaultLanguage,
             changeLanguage: () => new Promise(() => {}),
         };
     },
